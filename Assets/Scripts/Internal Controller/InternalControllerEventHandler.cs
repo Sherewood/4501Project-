e2054ee5fe5ef904268b30ebda5ad510a0b9492b
DannyMichaud <80365425+DannyMichaud@users.noreply.github.com>
2023-02-22 21:03:39 -0500

    FEATURE - Create skeleton of internal controller's event handler
    Defines a skeleton class for the Internal Controller's event handler, which will handle incoming events meant for Internal Controller classes

---
+++ Assets/Scripts/Internal Controller/InternalControllerEventHandler.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* Event definitions go here */
+
+//example definition for an event that passes an int
+/*
+[System.Serializable]
+public class ExampleEvent : UnityEvent<int> { }
+*/
+
+
+/* Internal Controller Class */
+// Handles incoming events to the Internal Controller
+
+
+public class InternalControllerEventHandler : MonoBehaviour
+{
+    // Controller classes
+
+
+    // Link other controller classes here
+    void Start()
+    {
+
+    }
+
+    // Event callback functions
+
+
+
+    // Helper functions
+}
