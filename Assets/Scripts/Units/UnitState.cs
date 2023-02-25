771065acf2c4beaf33559f51885503d402324bb3
DannyMichaud <80365425+DannyMichaud@users.noreply.github.com>
2023-02-24 21:32:55 -0500

    FEATURE - Add Unit State component
    Unit State component will hold the unit's state. You'd think it would be a state machine but it's just sorta going to work :frowning:

---
+++ Assets/Scripts/Units/UnitState.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/* Data type */
+//Purpose: Represents the state of the unit
+
+public enum UState
+{
+    STATE_IDLE,
+    STATE_MOVING
+}
+
+/* Unit class */
+//Purpose: Holds the state of the unit
+
+public class UnitState : MonoBehaviour
+{
+
+    private UState _state;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        _state = UState.STATE_IDLE;
+    }
+
+    public void SetState(UState state)
+    {
+        _state = state;
+    }
+
+    public UState GetState()
+    {
+        return _state;
+    }
+}
