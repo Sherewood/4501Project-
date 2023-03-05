e88ba0466b025ec0e23657bf53c50e6e3c26731b
Sherewood <52391353+Sherewood@users.noreply.github.com>
2023-03-05 17:57:08 -0500

    Creation of Uiabilities+texture images
    Feature- Added the abilitiy for selected units to load an indivdual button for each of their abilities, complete with onclick event
    Feature: textured Terrain
    Refactor:Julien
    Misc: Encountered bug where the selection seems to be pointing to nullpointer

---
+++ Assets/UiAbilties.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UiAbilties : MonoBehaviour
+{
+    public (string,UIEvTrigger) Event; //Trigger for button 
+    
+    public Sprite Icon; //button icon
+    private string Description; //string for later on which holds details 
+    // Start is called before the first frame update
+    void Start()
+    {
+       //give default ordereventvalue
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.GetComponent<Image>().sprite = Icon;
+    }
+    public void setTrigger((string, UIEvTrigger) odio)
+    {
+        Event=odio;
+    }
+    public void SendTrigger()
+    {
+        GameObject.Find("InternalController").GetComponent<InternalControllerEventHandler>().HandleUIOrderEvent(Event.Item1);
+    }
+    public void Seticon()
+    {
+        //sets the material 
+    }
+}
--- Assets/UserInterfaceController.cs
+++ Assets/UserInterfaceController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,11 +10,11 @@ public class UserInterfaceController : MonoBehaviour
 {
     public GameObject InternalController;
     public GameObject UnitInfo;
-    
+    public GameObject AbilPrefab;
     //copied from the selection controller 
 
     private List<GameObject> _selectedUnits;
-    private List<Capability> _selectedUnitCapabilities;
+    private Dictionary<string, UIEvTrigger> _selectedUnitCapabilities;
     private CapabilityController _capabilityController;
     public Material tracker;
     private DisplayInfoController  component;
@@ -31,6 +32,7 @@ public class UserInterfaceController : MonoBehaviour
     {
         // clearing function above
         _selectedUnits = component.GetSelectedUnits();
+        _selectedUnitCapabilities = component.GetSelectedUnitActions();
 
         if (_selectedUnits.Count >0) displayUnit();
 
@@ -38,16 +40,20 @@ public class UserInterfaceController : MonoBehaviour
     }
     void displayUnit()
     {
-        //commented stuff in regards to creating instances of unit blocks and placing them within the selection canvas 
-     //   for (int i = 0; i < _selectedUnits.Count; i++)
-      //  {
            
             //Debug.Log(Unitblock.transform.GetChild(0).transform.GetChild(0).name);
             UnitInfo.transform.GetChild(0).transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = _selectedUnits[0].GetComponent<UnitInfo>().UnitType;
-           
             UnitInfo.transform.GetChild(0).transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = _selectedUnits[0].GetComponent<Health>()._actualHealth.ToString()+ "/"+ _selectedUnits[0].GetComponent<Health>().MaxHealth.ToString(); //need a way to get health 
-                                                                                                                                                                                                                                               //Unitblock.transform.position = new Vector3(0 + i, 0,0);*/
-                                                                                                                                                                                                                                               //     }
-        UnitInfo.transform.GetChild(0).GetComponent<RawImage>().texture = component.GetUnitIcon(_selectedUnits[0].GetComponent<UnitInfo>().UnitType);
+             UnitInfo.transform.GetChild(0).GetComponent<RawImage>().texture = component.GetUnitIcon(_selectedUnits[0].GetComponent<UnitInfo>().UnitType);
+        //some really primitive attempt to place buttons from left to right
+        //ability display. 
+        int i = -20;
+        foreach (var ability in _selectedUnitCapabilities) 
+        {
+            GameObject abl = Instantiate(AbilPrefab);
+            abl.GetComponent<UiAbilties>().setTrigger((ability.Key, ability.Value));
+            abl.transform.position = new Vector3(i, 0, 0);
+            i += 10;
+        }
     }
 }
