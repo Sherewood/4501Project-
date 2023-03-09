df07a7fe6f573c0d855e374b1fe3f05c6b5ba4f7
DannyMichaud <80365425+DannyMichaud@users.noreply.github.com>
2023-03-08 22:30:15 -0500

    FIX - Update Mouse Controller to avoid handling mouse clicks in "UI Regions"
    The Mouse Controller will now check if a mouse is in any of the predefined "UI regions" - regions on the screen occupied by the UI, and avoid sending a SelectionEvent or MouseOrderEvent if it is.
    The current hard-coded storing of these UI-regions is poor design and will have to be updated. Either a better method of detecting UI elements, or a refactor to dynamically compute this in the UI controller, then send it to the mouse controller will have to be done, in order to avoid tedious updates to the UI regions when dimensions change, and allow for scaling the UI.

--- Assets/Scripts/External Controller/MouseController.cs
+++ Assets/Scripts/External Controller/MouseController.cs
@@ -28,6 +28,11 @@ public class MouseController : MonoBehaviour
     //mouse order event callback - right click
     public MouseOrderEvent MouseOrderEvent;
 
+    //temporary - should be set in UI for dynamic adjustment based on what menus are open
+    //denotes regions on the screen occupied by UI
+    //format is (x1, y1, x2, y2), with all x,y in range [0,1] where 0 is one end of the screen, and 1 is the other end
+    public List<Vector4> UIRegions;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -46,20 +51,28 @@ public class MouseController : MonoBehaviour
                 //if mouse button down, monitor mouse while held down using coroutine, and track held mouse button
                 if (Input.GetMouseButtonDown(i))
                 {
+                    if (IsMouseInUIRegion(Input.mousePosition))
+                    {
+                        Debug.Log("MouseController - click done in UI-occupied region. Ignore.");
+                        break;
+                    }
+
                     StartCoroutine(MonitorPressedMouse(i));
                     _mouseHeld = true;
                     _heldMouseButton = i;
 
                     //perform the initial raycast immediately, so shift in camera does not alter intended target
                     Ray initalMouseRay = gameCamera.ScreenPointToRay(Input.mousePosition);
+                    //if nothing hit, or a UI element is hit, ignore the mouse click and continue doing whatever
                     if (!Physics.Raycast(initalMouseRay, out _initialMouseSelection))
                     {
                         //if raycast doesn't work (due to no object being hit), mouse controller has no reason to continue monitoring this mouse selection
-                        Debug.Log("MouseController - No target for raycast on initial mouse click. Was possibly a UI element? Canceling mouse selection tracking...");
+                        Debug.Log("MouseController - No target for raycast on initial mouse click. Canceling mouse selection tracking...");
                         StopCoroutine(MonitorPressedMouse(i));
                         _mouseHeld = false;
                         _heldMouseButton = -1;
                     }
+
 
                     break;
                 }
@@ -76,8 +89,7 @@ public class MouseController : MonoBehaviour
 
                 //selection callback
                 if(_heldMouseButton == LEFT_MOUSE_BUTTON)
-                {
-                    
+                {         
                     SelectionEvent.Invoke(_initialMouseSelection);
                 }
                 //order callback
@@ -88,6 +100,32 @@ public class MouseController : MonoBehaviour
             }
         }
     }
+
+    //if mouse is in predefined 'UI region' of screen, then return true
+    //no doubt in my mind there's a better way of handling this, and this will probably fail in the future, but should work for now.
+    private bool IsMouseInUIRegion(Vector3 mousePosition)
+    {
+
+        Debug.Log(mousePosition);
+
+        //convert mouse position so it is in range [0-1,0-1] with 0 being start of screen, 1 being end of screen
+        //also invert mouse y coords because unity....
+        Vector2 mousePosConverted = new Vector2(mousePosition.x / Screen.width, (Screen.height - mousePosition.y) / Screen.height);
+
+        Debug.Log(mousePosConverted);
+
+        //check if in boundary
+        foreach (Vector4 UIRegion in UIRegions)
+        {
+            if((mousePosConverted.x >= UIRegion.x && mousePosConverted.x <= UIRegion.z) &&
+                (mousePosConverted.y >= UIRegion.y && mousePosConverted.y <= UIRegion.w))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
 
     //coroutine to monitor mouse while it is pressed
     //not in use for now.... will come into play for area selection...
