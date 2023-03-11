642541fe1d98a72575be41fdecda2918efbf9ff7
DannyMichaud <80365425+DannyMichaud@users.noreply.github.com>
2023-03-10 21:10:21 -0500

    FEATURE - Add camera movement acceleration, and isometric projection
    -Camera will now accelerate to a max speed thanks to Camera Controller
    -Camera is now oriented diagonally and is performing isometric projection (currently only done on component scene)

--- Assets/Scripts/Internal Controller/CameraController.cs
+++ Assets/Scripts/Internal Controller/CameraController.cs
@@ -9,16 +9,32 @@ public class CameraController : MonoBehaviour
 {
     public Camera GameCamera;
 
-    public float Speed = 1.0f;
+    //initial speed for the camera
+    public float BaseSpeed;
+
+    //maximum speed for the camera
+    public float MaxSpeed;
+
+    //accleration rate of the camera
+    public float Acceleration;
+
 
     private Dictionary<string, Vector3> directionMappings;
 
     //current movement direction of the camera
     private Vector3 _curDirection;
 
+    //previous movement direction of the camera
+    private Vector3 _prevDirection;
+
+    private Vector3 _curVelocity;
+
+
     void Start()
     {
         directionMappings = new Dictionary<string, Vector3>();
+        _curVelocity = new Vector3();
+        _curDirection = new Vector3();
 
         //set up direction mappings
         directionMappings.Add("none", new Vector3(0, 0, 0));
@@ -37,16 +53,52 @@ public class CameraController : MonoBehaviour
     {
         if(_curDirection != directionMappings["none"])
         {
-            Vector3 translation = _curDirection * Speed * Time.deltaTime;
+            //set velocity to base speed if velocity is below base speed
+            if (_curVelocity.magnitude < BaseSpeed)
+            {
+                _curVelocity = _curDirection * BaseSpeed;
+            }
+            //if direction changes, keep the same speed but in that direction
+            //might be a bit awkward but w/e
+            else if(_curDirection != _prevDirection)
+            {
+                _curVelocity = _curDirection * _curVelocity.magnitude;
+            }
+            else
+            {
+                //update velocity based on direction and acceleration
+                _curVelocity += _curDirection * Acceleration * Time.deltaTime;
+
+                //if speed exceeds maximum, flatten it to maximum
+                if(_curVelocity.magnitude > MaxSpeed)
+                {
+                    _curVelocity /= _curVelocity.magnitude;
+                    _curVelocity *= MaxSpeed;
+                }
+            }
+
+
+            Vector3 translation = _curVelocity * Time.deltaTime;
             /*
             the camera is rotated by a quaternion, so moving along the axis will not lead to
             the expected movement based on the direction vector. To move along the direction vector,
             need to apply the opposite of the quaternion's rotation to the translation vector, the two rotations
             will cancel each other out
             */
-            translation = Quaternion.Inverse(Camera.main.transform.rotation) * translation;
+            /*
+             however, with camera rotated on y axis, the static up/down orientation is also not what we want
+             Solution: Apply the y-axis rotation first, then apply the inverse rotation to rotate the translation by the y-axis rotation on its own
+            */
+            translation = Quaternion.Inverse(Camera.main.transform.rotation) * (Quaternion.AngleAxis(Camera.main.transform.rotation.eulerAngles.y, new Vector3(0, 1, 0)) * translation);
+            
             GameCamera.transform.Translate(translation);
         }
+        else
+        {
+            _curVelocity = Vector3.zero;
+        }
+
+        _prevDirection = _curDirection;
     }
 
     public void SetDirection(string direction)
