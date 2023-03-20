89e6182e37c0fb502f2783184560b8099546b4c9
Sherewood <52391353+Sherewood@users.noreply.github.com>
2023-03-20 19:01:35 -0400

    Controller modified
    Controller for soldier simplified

--- Assets/animation_Controller.cs
+++ Assets/animation_Controller.cs
@@ -9,6 +9,7 @@ public class animation_Controller : MonoBehaviour
     public Animator animator;
     private AnimatorControllerParameter[] parameter;
     public GameObject weapon;
+    public GameObject tool;
     //differenct animation functions 
     public bool gun=false;
     public bool heavy_gun=false;
@@ -30,6 +31,16 @@ public class animation_Controller : MonoBehaviour
     void Update()
     {
         if(animator.GetBool("FIRE")==false) weapon.SetActive(false);
+        if (animator.GetBool("HARVEST") == true)
+        {
+            tool.transform.GetChild(2).gameObject.SetActive(true);
+            tool.transform.GetChild(0).transform.Rotate(0, 20.0f, 0);
+        }
+        else if (animator.GetBool("HARVEST") == false)
+        {
+            tool.transform.GetChild(0).transform.Rotate(0, 0.0f, 0);
+            tool.transform.GetChild(2).gameObject.SetActive(false);
+        }
     }
    public  void SetAnim(String str)
     {
